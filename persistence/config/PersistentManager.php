fd1cf39bfc1152c5a73082b62c3939a26a85422f
javiab10 <javierbasoko@gmail.com>
2024-12-24 18:35:05 +0100

    Creación el proyecto
    - Creación de la estructura principal del proyecto.

---
+++ persistence/config/PersistentManager.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+/**
+ * Description of PersistentManager
+ *
+ * @author azpij
+ */
+class PersistentManager {
+    //put your code here
+}
